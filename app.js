b75e3b71f808ed090426614d445c8e7b911f7043
Kirti Choudhary <kirtimmd2@gmail.com>
2025-12-06 17:03:32 +0530

    updated database setup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV != "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
 
 const express = require("express");
@@ -10,52 +10,41 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
-const { error } = require('console');
 
 const dbUrl = process.env.ATLASDB_URL;
 
-main()
-    .then(() => {
-        console.log("Connected to database");
-    })
-    .catch(err => console.log(err));
-
 async function main() {
-    await mongoose.connect(dbUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000,
-    });
+    await mongoose.connect(dbUrl);
 }
+main().then(() => console.log("Connected to database"))
+    .catch(err => console.log("Database error:", err));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
+    crypto: { secret: process.env.SECRET },
     touchAfter: 24 * 3600,
 });
 
-store.on(error, () => {
-    console.log("Error on MongoDB Session store", err);
-})
+store.on("error", (err) => {
+    console.log("SESSION STORE ERROR:", err);
+});
 
 const sessionOptions = {
     store,
@@ -63,11 +52,12 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: true,
     cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpsOnly: true,
     },
-}
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -75,16 +65,15 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
     next();
-})
+});
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
@@ -92,7 +81,7 @@ app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not Found"));
-})
+});
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
@@ -101,4 +90,4 @@ app.use((err, req, res, next) => {
 
 app.listen(8080, () => {
     console.log("app is listening to port 8080");
-})
+});
